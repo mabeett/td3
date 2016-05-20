e97f4089057166d1132c21a01fcf63714d1c0202
Matías Pecchia <matiaspecchia@gridtics.frm.utn.edu.ar>
2016-05-20 10:27:57 -0300

    Clase 03 Invitación a invocar con argumentos

--- 03-linux/hola.c
+++ 03-linux/hola.c
@@ -7,5 +7,8 @@ int main(int arg_c, char ** arg_v)
 
 	for (i=1 ; i < arg_c ; i++)
 		printf("Argumento número %d es %s\n", i, arg_v[i]);
+
+	if (i==1)
+		printf("Intente ejecutar \"%s dos palabras\"\n", arg_v[0]);
 	return 0;
 }
