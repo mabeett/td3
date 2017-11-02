d70a2e036655685586584c881c9dc6795141c15a
Matías Pecchia <matiaspecchia@gridtics.frm.utn.edu.ar>
2017-11-02 00:21:26 -0300

    programa que evalua argumentos
    adios muestra por pantalla cada uno de los argumentos.
    Si el programa se ejecuta sin arguments se envía a stderr un mensaje
    de error y se retorna 1 en vez de 0

--- 03-linux/adios.c
+++ 03-linux/adios.c
@@ -1,8 +1,36 @@
+/*
+ * Programa de anexo. Se presenta por pantalla cada uno de los argumentos
+ * que recibe.
+ * En caso de no recibir argumentos se muestra por la salida de error un
+ * mensaje y se retorna 1.
+**/
+
 #include <stdio.h>
 
 int main(int arg_count, char **arg_values)
 {
-    printf("Hola Mundo!\n");
-    return 0;
+    int i, ret;
 
+    printf("Hola Mundo!\n");
+    printf(" Análisis de argumentos:\n");
+    printf("  arg_count vale %d\n", arg_count);
+
+    for (i = 0; i < arg_count; i++) {
+        printf("  arg_values[%d] vale '%s'.\n", i, arg_values[i]);
+    }
+
+    printf("Adios Mundo!\n");
+
+    /* Si no se enviaron argumentos retornar 1, sino 0
+     * Este programa espera argumentos, entonces si el usuario no los envía
+     * se retorna -1
+     **/
+    if (arg_count == 1) {
+        fprintf(stderr, "Por favor invoque a este programa con argumentos\n");
+        ret = 1;
+    } else {
+        ret = 0;
+    }
+
+    return ret;
 }
